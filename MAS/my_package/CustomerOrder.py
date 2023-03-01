823a8de484020d19d821cc2e925ed265c35f6ebf
Anirudh-aiti <126386656+Anirudh-aiti@users.noreply.github.com>
2023-03-01 23:45:25 +0530

    Add files via upload

---
+++ MAS/my_package/CustomerOrder.py
@@ -0,0 +1,8 @@
+class CustomerOrder:
+    def __init__(self, order_id, total_cost, order_list=""):
+        self.order_id = order_id
+        self.total_cost = total_cost
+        self.order_list = order_list
+
+    def thresholdCalculate(self):
+        f = input("dhasg")
